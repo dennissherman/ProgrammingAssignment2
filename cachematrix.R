9881542a69d0d245d6e40053ff2d8e118afd43fc
Dennis Sherman <drsynj-coursera@yahoo.com>
2015-09-20 12:47:54 -0500

    Improve test function and exercise all methods

--- cachematrix.R
+++ cachematrix.R
@@ -49,23 +49,35 @@ cacheSolve <- function(cacheMatrix, ...) {
     invMatrix
 }
 
-# test function, compare first and second run times
+# test function, compare first and second run times to show 
+# cache in use
 
-# theMatrix is an invertible matrix
-timer <- function(theMatrix) {
+# create a test matrix with random numbers
+# > set.seed(11235813)
+# > r = rnorm(1000000)
+# > mat1 = matrix(r, nrow=1000, ncol=1000)
+
+# > test(mat1)
+
+# matrix1 is an invertible matrix
+# expect first run of each matrix to take longer than repeats
+test <- function(matrix1) {
     
-    t <- makeCacheMatrix(theMatrix)
-    
+    t <- makeCacheMatrix(matrix1)
+
+    for ( i in 1:3) {
+        timer(t)
+    }
+    print("reset")
+    t$set(matrix1)
+    for ( i in 1:3) {
+        timer(t)
+    }
+}
+
+## utility to time execution
+timer <- function(cm) {
     start.time <- Sys.time()
-    cacheSolve(t)
-    print( Sys.time() - start.time)
-    
-    start.time <- Sys.time()
-    cacheSolve(t)
-    print( Sys.time() - start.time)
-    
-    start.time <- Sys.time()
-    cacheSolve(t)
-    print( Sys.time() - start.time)
-    
+    cacheSolve(cm)
+    print( Sys.time() - start.time )
 }
